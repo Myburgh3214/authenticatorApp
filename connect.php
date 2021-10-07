6ced96903a25b20e45e66c4399255f8163362a99
Myburgh3214 <79691187+Myburgh3214@users.noreply.github.com>
2021-10-07 12:29:47 +0200

    Online db setup
    Fully working awaiting marking

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
     // Storing our mysqli class parameters in variables
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    $dbname = "library";
+    $servername = "sql11.freesqldatabase.com";
+    $username = "sql11442742";
+    $password = "TP5SVkZaCf";
+    $dbname = "sql11442742";
 
     // Using the above variables as parameter values
     $conn = new mysqli($servername, $username, $password, $dbname); // Create connection
