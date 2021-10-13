d828d64990bbd7294ce7cbe6c051ff54eae768b5
Myburgh3214 <79691187+Myburgh3214@users.noreply.github.com>
2021-10-13 10:33:36 +0200

    Gallery is working
    Gallery is working

--- signIn.php
+++ signIn.php
@@ -49,7 +49,7 @@ include ("connect.php");
  <div class="w3-bar w3-theme-d2 w3-left-align">
     <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-hover-white w3-theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
     <a href="index.php" class="w3-bar-item w3-button w3-teal"><i class="fa fa-home w3-margin-right"></i>Home</a>
-    <a href="signUplog.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Sign Up</a>
+    <a href="signUplog.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Librarian Menu</a>
     <a href="forgot.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Forgot Password</a>
  </div>
 </div>
